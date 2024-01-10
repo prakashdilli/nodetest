a9c849083d51aa3dc58c7fa7ef815e645852e80a
Prakash Dillibabu <prakash.d@finstein.ai>
2024-01-10 18:59:41 +0530

    just added env

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     const myEnvVar = process.env.dbconnectionstring || 'Environment variable not set';
-    res.send(`Environment Variable: ${myEnvVar} and the Port is ${port}`);
+    const myEnvVar1 = process.env.testingenv || 'Environment variable not set';
+    res.send(`Environment Variable: ${myEnvVar} and the Port is ${port} another env ${myEnvVar1} `);
 });
 
 app.listen(port, () => {
