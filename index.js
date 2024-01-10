38418eab079ce74ed0d49a7601ecfb5bb7a2fca9
Prakash Dillibabu <prakash.d@finstein.ai>
2024-01-10 18:21:21 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    const myEnvVar = process.env.dbconnectionstring || 'Environment variable not set';
+    res.send(`Environment Variable: ${myEnvVar}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
