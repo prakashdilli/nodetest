9dfefb6e984f9a26c51eddcf29e8d322cbe17336
Prakash Dillibabu <prakash.d@finstein.ai>
2024-01-10 19:04:49 +0530

    reverted the additional env

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     const myEnvVar = process.env.dbconnectionstring || 'Environment variable not set';
-    const myEnvVar1 = process.env.testingenv || 'Environment variable not set';
-    res.send(`Environment Variable: ${myEnvVar} and the Port is ${port} another env ${myEnvVar1} `);
+    res.send(`Environment Variable: ${myEnvVar} and the Port is ${port}`);
 });
 
 app.listen(port, () => {
